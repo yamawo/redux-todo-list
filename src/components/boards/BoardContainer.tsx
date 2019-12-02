473638889afbc3c936af7405f48b248ca66b0ab9
yamawo <y7a3m1asaki@gmail.com>
2019-12-02 22:46:01 +0900

    created BoardContainer.tsx

--- src/components/boards/BoardContainer.tsx
+++ src/components/boards/BoardContainer.tsx
@@ -3,13 +3,35 @@ import styled from 'styled-components';
 import { connect } from 'react-redux';
 import { fadeIn } from '../../Variables/Animation';
 
-class BoardContainer extends Component {
+class BoardContainer extends React.Component {
+
+    renderAllBoards = () => {
+        const { boardsCollection } = this.props;
+            return boardsCollection.map( board => {
+                return (
+                    <ShowAllBoards
+                        id={board.id}
+                        key={board.id}
+                        title={board.title}
+                    />
+                )
+            })
+    }
 
     render() {
         return (
             <Wrapper>
-                
+                <CreateBoardContainer />
+                {this.renderAllBoards()}
             </Wrapper>
         )
     }
-}
+};
+
+function mapStateToProps({ boardsCollection }) {
+    return (
+        boardsCollection
+    )
+};
+
+export default connect(mapStateToProps)(BoardContainer);
