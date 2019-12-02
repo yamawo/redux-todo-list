cd264b5e36874413b7fb8cce1f3aa9cfde994fb0
yamawo <y7a3m1asaki@gmail.com>
2019-12-03 00:08:36 +0900

    created CreateBoardContainer.tsx

---
+++ src/components/boards/boardCreation/CreateBoardContainer.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import { connect } from 'react-redux';
+import CreateBoard from './CreateBoard';
+import ActiveCreateBoard from './ActiveCreateBoard';
+
+interface Props {
+    newBoard: object;
+}
+
+const CreateBoardContainer = ({ newBoard }: Props) => (
+    <div>
+        { newBoard.isBoardOpen ? <ActiveCreateBoard /> : <CreateBoard /> }
+    </div>
+);
+
+function mapStateToProps({ newBoard }) {
+    return {
+        newBoard
+    }
+}
+
+export default connect(mapStateToProps)(CreateBoardContainer);
